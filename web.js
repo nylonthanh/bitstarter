1d6d2ad3d0fd3f6f533e40b4b8e08922acabc8e3
Tarsisius J <thanh@thismoment.com>
2013-07-11 05:11:08 +0000

    update web.js

--- web.js
+++ web.js
@@ -2,11 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-
+var buf = new Buffer(256);
 
 app.get('/', function(request, response) {
-    fs.readFileSync('index.html');
-});
+  var res = buf.write(fs.readFile('./index.html', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
+  response.send(res);
+
+}););
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
