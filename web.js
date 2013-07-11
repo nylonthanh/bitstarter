b1902b6bd94ca7c09731074daaef6ae0deed775b
Tarsisius J <thanh@thismoment.com>
2013-07-11 05:24:04 +0000

    revert

--- web.js
+++ web.js
@@ -1,13 +1,10 @@
 var express = require('express');
-//var fs = require('fs');
 
 var app = express.createServer(express.logger());
-//var buf = new Buffer(256);
 
 app.get('/', function(request, response) {
-  response.send('start again');
-
-}););
+  response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
