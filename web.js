46ed7f4b1dc923b979a03be0469ffabe290fa66a
nylonthanh <thanh@thismoment.com>
2013-07-22 03:10:26 +0000

    update

--- web.js
+++ web.js
@@ -1,25 +1,15 @@
-/* edited for dev */
 var express = require('express');
 var fs = require('fs');
-var rest = require('restler');
-var cheerio = require('cheerio');
+var htmlfile = "index.html";
 
-var buf = new Buffer(fs.readFileSync('index.html'));
 var app = express.createServer(express.logger());
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 app.get('/', function(request, response) {
-    rest.get(apiurl).on('complete', response2console);
-    response.send(buf.toString('utf8'));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-}
-
